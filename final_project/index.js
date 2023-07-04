a207bf6dce48035a63c9de18d71bd287f2db4719
yonishi-13 <127645955+yonishi-13@users.noreply.github.com>
2023-07-04 21:31:08 +0530

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -5,15 +5,41 @@ const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
 const app = express();
+const username = ["Mitali","OKOK"];
+const password = ["123","wow"];
 
+ 
 app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+if(req.session.authorization) {
+    token = req.session.authorization[ 'accessToken'];
+    jwt.verify(token,"access",(err,user)=>{
+        if(!err)
+        {
+            req.user = user;
+            next();
+        }
+        else{
+            return res.status(403).json({message: "User not authenticated"})
+        }
+     });
+ } else {
+     return res.status(403).json({message: "User not logged in"})
+ }
+    
 });
- 
+
+app.post("/", (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    res.send("User loggged in!");
+    
+});
+
 const PORT =5000;
 
 app.use("/customer", customer_routes);
