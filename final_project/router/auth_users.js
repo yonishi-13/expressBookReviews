261fffb635447759b81a38920e2b0b4f46e025e4
yonishi-13 <127645955+yonishi-13@users.noreply.github.com>
2023-07-04 21:31:46 +0530

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,26 +5,48 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+const doesExist = (username)=>{
+    let userswithsamename = users.filter((user)=>{
+      return user.username === username
+    });
+    if(userswithsamename.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
+
+ 
+    const authenticatedUser = (username,password)=>{
+        let validusers = users.filter((user)=>{
+          return (user.username === username && user.password === password)
+        });
+        if(validusers.length > 0){
+          return true;
+        } else {
+          return false;
+        }
+      }//returns boolean
 //write code to check if username and password match the one we have in records.
-}
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+    res.send("User succesfully logged in!");
 });
+  
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
+
+  res.send("Your review has been added");
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
+
 module.exports.authenticated = regd_users;
-module.exports.isValid = isValid;
+
 module.exports.users = users;
